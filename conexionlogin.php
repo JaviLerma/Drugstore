4f120f088f51335dd00b576b5db2acabe9eb5f2c
JaviLerma <41553977+JaviLerma@users.noreply.github.com>
2019-04-04 01:03:30 -0300

    cambio a drugstore1 db

--- conexionlogin.php
+++ conexionlogin.php
@@ -1,5 +1,5 @@
 <?php
-$conexiondb=new mysqli("localhost","root","","drugstore");
+$conexiondb=new mysqli("localhost","root","","drugstore1");
 	if($conexiondb->connect_error){
 		die("Ocurrió un error al intentar conectar la db");
 	}
--- main.php
+++ main.php
@@ -3,12 +3,12 @@
 	require 'conexionlogin.php';
 	$usuario = $_GET["nUsuario"];
 	$password = $_GET["nPassword"];
-	$sql = "Select U.pass from Usuarios U Where U.usuario = '".$usuario."'";
+	$sql = "Select U.contrasenia from usuarios U Where U.usuario = '".$usuario."'";
 	//$sql = "Select U.Contrasenia from Usuario U Where U.Nombre = 'admin'";
 	$resultadoQuery = $conexiondb->query($sql);
 	if($resultadoQuery->num_rows > 0){
 		$fila = $resultadoQuery->fetch_assoc();
-		if($password == $fila["pass"]){
+		if($password == $fila["contrasenia"]){
 			$_SESSION['logueado'] = true;
 			$_SESSION['usuario'] = $usuario;
 			header("Location: http://localhost/index.php");
